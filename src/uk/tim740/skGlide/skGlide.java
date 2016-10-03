be56352052b1fc446feabb40956afabcfa5afd98
tim740 <tim740x@gmail.com>
2016-10-03 14:19:18 +0100

    stuff

---
+++ src/uk/tim740/skGlide/skGlide.java
@@ -0,0 +1,21 @@
+package uk.tim740.skGlide;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.util.SimpleEvent;
+import org.bukkit.event.entity.EntityToggleGlideEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by tim740 on 03/10/2016
+ */
+
+public class skGlide extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Skript.registerAddon(this);
+        Skript.registerExpression(SExprGlideMode.class, Boolean.class, ExpressionType.PROPERTY, "glide (state|ability|mode) of %entity%", "%entity%'s glide (state|ability|mode)");
+        Skript.registerEvent("GlideToggle", SimpleEvent.class, EntityToggleGlideEvent.class, "[elytra ]glide toggle");
+    }
+}
